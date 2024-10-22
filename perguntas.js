8d368fb04b14f31a829db3d85aa2a80d214b7445
vsbruna <141277861+vsbruna@users.noreply.github.com>
2024-10-22 09:08:53 -0300

    h

--- perguntas.js
+++ perguntas.js
@@ -5,9 +5,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Camisa 10',
+    'Quem é o camisa 10 do Palmeias?',
+    'Rony'
 )
 
 criaCartao(
